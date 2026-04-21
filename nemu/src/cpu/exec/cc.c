ec7c964e19e4802d1c633f6bada496093d61d6f9
2312236 Jiaquan Fu <2382175384@qq.com>
2026-04-21 16:56:09 +0800

    [implement] rtl_setcc

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -11,17 +11,46 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_L, CC_NL, CC_LE, CC_NLE
   };
 
-  // TODO: Query EFLAGS to determine whether the condition code is satisfied.
+  // TODO(finish): Query EFLAGS to determine whether the condition code is satisfied.
+  // 检查 subcode 的低 3 位，确定要检查哪个条件码，并根据相应的条件码来查询 EFLAGS 寄存器中的标志位，判断条件是否满足
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
+      // 溢出标志位 OF
+      rtl_get_OF(dest);
+      break;
     case CC_B:
+      // 进位标志位 CF
+      rtl_get_CF(dest);
+      break;
     case CC_E:
+      // 零标志位 ZF
+      rtl_get_ZF(dest);
+      break;
     case CC_BE:
+      // 进位标志位 CF 或者 零标志位 ZF
+      rtl_get_CF(&t0);
+      rtl_get_ZF(&t1);
+      rtl_or(dest, &t0, &t1);
+      break;
     case CC_S:
+      // 符号标志位 SF
+      rtl_get_SF(dest);
+      break;
     case CC_L:
+      // 符号标志位 SF 和 溢出标志位 OF 不相等
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_xor(dest, &t0, &t1);
+      break;
     case CC_LE:
-      TODO();
+      // 符号标志位 SF 和 溢出标志位 OF 不相等，或者 零标志位 ZF
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_xor(&t0, &t0, &t1);
+      rtl_get_ZF(&t1);
+      rtl_or(dest, &t0, &t1);
+      break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
