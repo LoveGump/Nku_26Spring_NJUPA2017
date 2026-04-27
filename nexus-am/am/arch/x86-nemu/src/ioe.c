2fbfadcaa95a65f083e1bf2a8f5e0f803f9584ea
tracer-ics2017 <tracer@njuics.org>
2026-04-27 22:59:21 +0800

    >  run 2312236 gump Linux 2204 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:59:21 up  1:21,  1 user,  load average: 2.13, 2.35, 2.34 93f172cc5d3469a47506ca9b7b6ff273aa917dda

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -24,40 +24,45 @@ extern void* memcpy(void *, const void *, int);
 
 // 实现 _draw_rect() 函数，将 pixels 中的像素数据绘制到屏幕上指定的位置
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  int src_stride = w;
-  int src_x = 0, src_y = 0;
+  int i;
+  for (i = 0; i < _screen.width * _screen.height; i++) {
+    fb[i] = i;
+  }
+  
+  // int src_stride = w;
+  // int src_x = 0, src_y = 0;
 
-  if (w <= 0 || h <= 0) {
-    return;
-  }
+  // if (w <= 0 || h <= 0) {
+  //   return;
+  // }
 
-  if (x < 0) {
-    src_x = -x;
-    w += x;
-    x = 0;
-  }
-  if (y < 0) {
-    src_y = -y;
-    h += y;
-    y = 0;
-  }
-  if (w <= 0 || h <= 0 || x >= _screen.width || y >= _screen.height) {
-    return;
-  }
+  // if (x < 0) {
+  //   src_x = -x;
+  //   w += x;
+  //   x = 0;
+  // }
+  // if (y < 0) {
+  //   src_y = -y;
+  //   h += y;
+  //   y = 0;
+  // }
+  // if (w <= 0 || h <= 0 || x >= _screen.width || y >= _screen.height) {
+  //   return;
+  // }
 
-  if (x + w > _screen.width) {
-    w = _screen.width - x;
-  }
-  if (y + h > _screen.height) {
-    h = _screen.height - y;
-  }
+  // if (x + w > _screen.width) {
+  //   w = _screen.width - x;
+  // }
+  // if (y + h > _screen.height) {
+  //   h = _screen.height - y;
+  // }
 
-  int copy_bytes = w * sizeof(uint32_t);
-  const uint32_t *src = pixels + src_y * src_stride + src_x;
-  for (int j = 0; j < h; j++) {
-    memcpy(&fb[(y + j) * _screen.width + x], src, copy_bytes);
-    src += src_stride;
-  }
+  // int copy_bytes = w * sizeof(uint32_t);
+  // const uint32_t *src = pixels + src_y * src_stride + src_x;
+  // for (int j = 0; j < h; j++) {
+  //   memcpy(&fb[(y + j) * _screen.width + x], src, copy_bytes);
+  //   src += src_stride;
+  // }
 }
 
 // 用于将之前的绘制内容同步到屏幕上 (在NEMU中绘制内容总是会同步到屏幕上, 因而无需实现此API)
