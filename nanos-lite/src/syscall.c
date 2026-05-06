a68dcb3085f1597aa042f387eb8a8ad790794f89
2312236 Jiaquan Fu <2382175384@qq.com>
2026-05-06 12:27:41 +0800

    Handle SYS_write

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,10 +5,24 @@ _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
   a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
     case SYS_none:
       SYSCALL_ARG1(r) = 1;
+      break;
+    case SYS_write:
+      if (a[1] == 1 || a[1] == 2) {
+        char *buf = (char *)a[2];
+        size_t len = a[3];
+        for (size_t i = 0; i < len; i ++) {
+          _putc(buf[i]);
+        }
+        SYSCALL_ARG1(r) = len;
+      } else {
+        SYSCALL_ARG1(r) = -1;
+      }
       break;
     case SYS_exit:
       _halt(a[1]);
