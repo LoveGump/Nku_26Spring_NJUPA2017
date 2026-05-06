c4b6db32fd5e4285c216bd5a574b85503823000e
2312236 Jiaquan Fu <2382175384@qq.com>
2026-05-06 13:03:48 +0800

    Implement framebuffer write

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -18,6 +18,27 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+  assert(offset % sizeof(uint32_t) == 0);
+  assert(len % sizeof(uint32_t) == 0);
+
+  const uint32_t *pixels = (const uint32_t *)buf;
+  size_t pixel_offset = offset / sizeof(uint32_t);
+  size_t pixels_left = len / sizeof(uint32_t);
+
+  while (pixels_left > 0) {
+    int x = pixel_offset % _screen.width;
+    int y = pixel_offset / _screen.width;
+    size_t row_pixels = _screen.width - x;
+    if (row_pixels > pixels_left) {
+      row_pixels = pixels_left;
+    }
+
+    _draw_rect(pixels, x, y, row_pixels, 1);
+
+    pixels += row_pixels;
+    pixel_offset += row_pixels;
+    pixels_left -= row_pixels;
+  }
 }
 
 void init_device() {
