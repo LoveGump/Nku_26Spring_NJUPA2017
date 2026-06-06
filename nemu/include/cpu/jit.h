a30e409abf5d0bea3c0e84c2d6492e836f05c72d
2312236 Jiaquan Fu <2382175384@qq.com>
2026-06-06 10:42:21 +0800

    pa5: add translation block cache

--- nemu/include/cpu/jit.h
+++ nemu/include/cpu/jit.h
@@ -5,9 +5,34 @@
 
 #ifdef CONFIG_JIT
 
+#define JIT_TB_CACHE_SIZE 4096
+
+typedef struct TranslationBlock {
+  bool valid;
+  vaddr_t guest_start;
+  vaddr_t guest_end;
+  uint32_t nr_instr;
+  uint64_t hit_count;
+  void *host_code;
+  uint32_t host_size;
+} TB;
+
+typedef struct {
+  uint64_t lookups;
+  uint64_t hits;
+  uint64_t misses;
+  uint64_t translations;
+  uint64_t invalidations;
+  uint64_t total_instr;
+} JITStats;
+
 void jit_init(void);
 void jit_reset(void);
 void jit_invalidate_all(void);
+TB *tb_lookup(vaddr_t eip);
+TB *tb_alloc(vaddr_t eip);
+void tb_invalidate(TB *tb);
+const JITStats *jit_get_stats(void);
 
 #else
 
--- nemu/src/cpu/jit/jit.c
+++ nemu/src/cpu/jit/jit.c
@@ -4,14 +4,20 @@
 
 typedef struct {
   bool enabled;
-  uint64_t invalidations;
+  TB tb_cache[JIT_TB_CACHE_SIZE];
+  JITStats stats;
 } JITState;
 
 static JITState jit_state;
 
+static inline uint32_t tb_index(vaddr_t eip) {
+  return (eip >> 2) & (JIT_TB_CACHE_SIZE - 1);
+}
+
 void jit_init(void) {
   jit_state.enabled = true;
-  jit_state.invalidations = 0;
+  memset(&jit_state.stats, 0, sizeof(jit_state.stats));
+  jit_invalidate_all();
 }
 
 void jit_reset(void) {
@@ -19,7 +25,55 @@ void jit_reset(void) {
 }
 
 void jit_invalidate_all(void) {
-  jit_state.invalidations ++;
+  for (int i = 0; i < JIT_TB_CACHE_SIZE; i ++) {
+    tb_invalidate(&jit_state.tb_cache[i]);
+  }
+  jit_state.stats.invalidations ++;
+}
+
+TB *tb_lookup(vaddr_t eip) {
+  jit_state.stats.lookups ++;
+
+  TB *tb = &jit_state.tb_cache[tb_index(eip)];
+  if (tb->valid && tb->guest_start == eip) {
+    tb->hit_count ++;
+    jit_state.stats.hits ++;
+    return tb;
+  }
+
+  jit_state.stats.misses ++;
+  return NULL;
+}
+
+TB *tb_alloc(vaddr_t eip) {
+  TB *tb = &jit_state.tb_cache[tb_index(eip)];
+  tb_invalidate(tb);
+
+  tb->valid = true;
+  tb->guest_start = eip;
+  tb->guest_end = eip;
+  tb->nr_instr = 0;
+  tb->hit_count = 0;
+  tb->host_code = NULL;
+  tb->host_size = 0;
+  jit_state.stats.translations ++;
+
+  return tb;
+}
+
+void tb_invalidate(TB *tb) {
+  if (tb == NULL) {
+    return;
+  }
+
+  if (tb->valid) {
+    jit_state.stats.total_instr += tb->nr_instr;
+  }
+  memset(tb, 0, sizeof(*tb));
+}
+
+const JITStats *jit_get_stats(void) {
+  return &jit_state.stats;
 }
 
 #endif
